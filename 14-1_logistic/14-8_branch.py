ef2ba9ab9ba8644ac0de361390a12fc8bcc30c45
kei_mac-mini <gerogero7429@gmail.com>
2020-05-22 19:56:21 +0900

    14-8_branch.py

---
+++ 14-1_logistic/14-8_branch.py
@@ -0,0 +1,37 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import japanize_matplotlib
+
+N = 300     # N+200: 経過させる世代数
+
+# グラフ描画用変数
+xlist = []
+ylist = []
+
+print('start')
+for mu in np.arange(1, 4, 0.003):
+    #print(mu)
+    for x0 in np.arange(0.002, 1, 0.002):
+        x = x0
+        # 始めに200世代経過させる
+        for i in range(1, 200):
+            x = mu * x * (1-x)
+        for i in range(1, N):
+            x_before = x
+            x = mu * x * (1-x)
+            if x_before == x:
+                break
+        ylist.append(float(format(x, '.4f')))
+        #ylist.append(x)
+        xlist.append(mu)
+
+print('グラフ描画開始')
+fig = plt.figure(figsize=(15, 15))
+plt.title('分岐図')
+plt.scatter(xlist, ylist, s=0.3)
+plt.xlabel('$\mu$')
+plt.ylabel('$x_*$')
+fig.savefig('14-1_logistic/branch.png', dpi=300)   # 画像の保存
+plt.show()
+
+print('グラフ描画終了')
