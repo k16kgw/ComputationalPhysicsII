d5708a4c106498dd0aed34bad7683c93f2473dab
kei_mac-mini <gerogero7429@gmail.com>
2020-05-22 21:42:42 +0900

    branch2.py

--- 14-1_logistic/14-8_branch_2.py
+++ 14-1_logistic/14-8_branch_2.py
@@ -2,7 +2,7 @@
 """
 14-8_branch_2.pyプログラム
 Landau, et al. 『計算物理学II』朝倉書店．14章p.343の演習
-（無次元化された）ロジスティック写像の分岐図をμが3から4の範囲で描画するシミュレーション
+ロジスティック写像の分岐図をμが3から4の範囲で描画するシミュレーション
 使い方 14-8_branch_2.py
 """
 
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import japanize_matplotlib
 
-N = 300     # N+200: 経過させる世代数
+N = 300     # N+200:経過させる世代数
 
 # グラフ描画用変数
 xlist = []
@@ -39,7 +39,7 @@ plt.title('分岐図')
 plt.scatter(xlist, ylist, s=0.3)
 plt.xlabel('$\mu$')
 plt.ylabel('$x_*$')
-fig.savefig('14-1_logistic/branch.png', dpi=300)   # 画像の保存
+fig.savefig('14-1_logistic/branch_3-4.png', dpi=300)   # 画像の保存
 plt.show()
 
 print('グラフ描画終了')
